7395e98a1a4e7160cb4ae2258c1331897e55ec2c
meryemkaftar <merykft@gmail.com>
2023-12-17 19:22:55 +0300

    whack-a-mole game

---
+++ Whack-A-Mole/script.js
@@ -0,0 +1,70 @@
+// look after all the elements that have 'square' on them
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const startGame = document.querySelector(".start-game");
+
+// pick timer and the score elements
+const timer = document.getElementById("timer");
+const score = document.getElementById("score");
+
+// boolean flag. always start with a boolean flag
+let result = 0;
+let hitPosition;
+let currentTime = 30;
+let timerId = null;
+
+const randomSquare = () => {
+  // step 1. only one square pe moment can have the mole
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  // step 2. get a random position
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+
+  // step 3. add the mole (based on step 2's randomness)
+  randomSquare.classList.add("mole");
+
+  // get the id of the randomly chosen square
+  // and put that inside the hit position
+  hitPosition = randomSquare.id;
+};
+
+squares.forEach((square) => {
+  // let's add an event listener for each square (which is an array)
+  square.addEventListener("mousedown", () => {
+    // add point with each click
+    // we are getting the id of the square (from the html element)
+    // and we compare it to the hitPosition
+    // the hitPosition is set to be equal to the randomSquare's id (which we get from the other function)
+
+    // if that is true (and they match), then return a point
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+const moveMoleAround = () => {
+  // setInterval(function, timeInterval)
+  timerId = setInterval(randomSquare, 500);
+
+  currentTime--;
+  timer.textContent = currentTime--;
+};
+
+const countDown = () => {
+  if (currentTime === 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("GAME OVER. Your final score is " + result);
+  }
+};
+
+let countDownTimerId = setInterval(countDown, 1000);
+
+// moveMoleAround();
+startGame.addEventListener("click", moveMoleAround);
